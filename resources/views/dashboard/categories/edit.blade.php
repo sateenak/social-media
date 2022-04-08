f761ede528dff69c157a8caaeaf68f342bcb7577
unknown <marceldwias@gmail.com>
2022-04-08 14:54:25 +0700

    update composer 2

--- resources/views/dashboard/categories/edit.blade.php
+++ resources/views/dashboard/categories/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('dashboard.layouts.main')
 @section('container')
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-    <h1 class="h2">Edit Post</h1>
+    <h1 class="h2">Edit Postingan</h1>
   </div>
   <div class="col-lg-8">
   <form method="POST" action="/dashboard/categories/{{ $category->slug }}" class="mb-5" enctype="multipart/form-data">
